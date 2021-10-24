776f0a2dda75291c8a881c0c9488f5ae8558d041
JanUlc <sru799@gmail.com>
2021-10-24 18:03:43 +0200

    initial commit

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users = [
+    {
+        name: 'Barbara Klisz',
+        attendance: '40%',
+        average: '2.3'
+    },
+    {
+        name: 'Adam Karol',
+        attendance: '45%',
+        average: '4.1'
+    },
+    {
+        name: 'Jan Bicz',
+        attendance: '20%',
+        average: '3.7'
+    },
+    {
+        name: 'Radoslaw Klisz',
+        attendance: '37%',
+        average: '2.3'
+    },
+    {
+        name: 'Tomasz Kowalski',
+        attendance: '65%',
+        average: '2.5'
+    },
+    {
+        name: 'Jaroslaw Dziwisz',
+        attendance: '15%',
+        average: '2.1'
+    },
+    {
+        name: 'Krzysztof Szef',
+        attendance: '73%',
+        average: '4.1'
+    },
+]
---
+++ src/views/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UserList from '../components/UserList/UsersList';
+
+const Root = () => (
+  <>
+    <UserList />
+  </>
+);
+
+Root.propTypes = {};
+
+export default Root;
